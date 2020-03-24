7cf78b49d0eb98121b05874241f2aa0abc7c6df1
Tom Schaum <devilgoof@DESKTOP-15C8896.localdomain>
2020-03-23 16:40:58 -1000

    skeleton-react-app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Here's a basic layout
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        } 
+    }
+
+    render() {
+        return (
+            <div className="App">
+                Here's a basic layout
+            </div>
+        )
+    }
 }
+
 
 export default App;
